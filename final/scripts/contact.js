f00afa757ddc0eb2826023c2cd3555c134ad6e21
chihchunhsiao <chsiao@byupathway.edu>
2025-10-11 15:07:28 +0800

    delete modal.css

--- final/scripts/contact.js
+++ final/scripts/contact.js
@@ -9,7 +9,7 @@ console.log(myInfo.get('last'));
 console.log(myInfo.get('phone'));
 console.log(myInfo.get('email'));
 console.log(myInfo.get('organization'));
-console.log(myInfo.get('formLoadTime'));
+
 
 
 // get id="results" from thanks.html and set the contents of results
@@ -17,6 +17,5 @@ document.querySelector('#results').innerHTML = `<p>Application for ${myInfo.get(
 <p>Your Phone: ${myInfo.get('phone')}</p>
 <p>Your email is: ${myInfo.get('email')}</p>
 <p>Your Organization: ${myInfo.get('organization')} </p>
-<p>Submission Date: ${myInfo.get('formLoadTime')} </p>
 `;
 
